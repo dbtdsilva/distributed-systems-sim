d86885194eaf0ead73d2647bc4534f5eee6753de
guesswho <guesswho@guesswhat>
2015-03-29 19:05:37 +0100

    Fixed log file name.

--- ArtesanatoAveiro/src/Exec/ArtesanatoAveiro.java
+++ ArtesanatoAveiro/src/Exec/ArtesanatoAveiro.java
@@ -21,7 +21,7 @@ public class ArtesanatoAveiro {
      * @param args the command line arguments
      */
     public static void main(String[] args) { 
-        Logging log = new Logging("logging.txt", 
+        Logging log = new Logging("", 
                 ProbConst.nCustomers, 
                 ProbConst.nCraftsmen,
                 ProbConst.nPrimeMaterials);
