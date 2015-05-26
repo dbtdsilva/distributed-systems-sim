cd2b476cad5b21b76457836bb3df22847f8cd8a9
Diogo Silva <dbtds@ua.pt>
2015-05-26 17:02:08 +0100

    * RegistryConst

--- ArtesanatoAveiro/src/Constants/RegistryConst.java
+++ ArtesanatoAveiro/src/Constants/RegistryConst.java
@@ -10,5 +10,23 @@ package Constants;
  * @author diogosilva
  */
 public class RegistryConst {
+    public static String logNameEntry = "LoggingInt";
+    public static String shopNameEntry = "ShopInt";
+    public static String workshopNameEntry = "WorkshopInt";
+    public static String warehouseNameEntry = "WarehouseInt";
     
+    public static int portRegistry = 22440; // 22440 - 22449
+    public static String hostRegistry = "localhost";
+    
+    public static int portLogging = 22441;
+    public static String hostLogging = "localhost";
+    
+    public static int portShop = 22442;
+    public static String hostShop = "localhost";
+    
+    public static int portWorkshop = 22443;
+    public static String hostWorkshop = "localhost";
+    
+    public static int portWarehouse = 22444;
+    public static String hostWarehouse = "localhost";
 }
